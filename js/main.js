908b951f660da26f2e4efac4a14e18d06a3f9852
Mikolaj <paniol296@gmail.com>
2026-06-30 15:09:32 +0200

    Initial version of VO Nails Atelier website

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const links = document.querySelectorAll('a[href^="#"]');
+
+links.forEach(link => {
+
+    link.addEventListener("click", function (e) {
+
+        const target = document.querySelector(this.getAttribute("href"));
+
+        if (!target) return;
+
+        e.preventDefault();
+
+        target.scrollIntoView({
+
+            behavior: "smooth"
+
+        });
+
+    });
+
+});
